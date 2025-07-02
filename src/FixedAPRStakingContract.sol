29f666c2ce7c32d31728896d1d572ced52f9e8b2
timburman <aryankaushik251@gmail.com>
2025-07-02 20:09:26 +0530

    Fix: Format

--- src/FixedAPRStakingContract.sol
+++ src/FixedAPRStakingContract.sol
@@ -305,7 +305,9 @@ contract FixedAPRStakingContract is Ownable, ReentrancyGuard {
         require(baseAPRFor365Days > 0 && baseAPRFor365Days <= MAX_APR, "Invalid base APR");
 
         if (rewardPool.lastAPRChange != 0) {
-            require(block.timestamp >= rewardPool.lastAPRChange + APR_CHANGE_COOLDOWN, "APR change cooldown not completed");
+            require(
+                block.timestamp >= rewardPool.lastAPRChange + APR_CHANGE_COOLDOWN, "APR change cooldown not completed"
+            );
         }
         require(baseAPRFor365Days != rewardPool.baseAPRFor365Days, "APR already set to this value");
         rewardPool.baseAPRFor365Days = baseAPRFor365Days;

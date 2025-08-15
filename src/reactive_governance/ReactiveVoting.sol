dcc31da501a1d6b57269ebcc98bb3a9d66ae5d7b
timburman <aryankaushik251@gmail.com>
2025-08-15 15:53:27 +0530

    ReactiveGovernance: ReactiveVoting contract process started

---
+++ src/reactive_governance/ReactiveVoting.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "./ReactiveStaking.sol";
+
+abstract contract ReactiveVoting is Initializable, ReentrancyGuardUpgradeable {
+    
+}
